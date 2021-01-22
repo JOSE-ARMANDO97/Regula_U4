9879866f6ae0b85bb72586fa144551e1d6db275f
JOSE-ARMANDO97 <71796689+JOSE-ARMANDO97@users.noreply.github.com>
2021-01-21 18:01:07 -0600

    Create Nodo.java

---
+++ Nodo/Nodo.java
@@ -0,0 +1,14 @@
+package Nodo;
+
+
+public class Nodo {
+    public Nodo izquierda;
+   public  Nodo Derecha;
+     public  int elemento;
+
+    public Nodo(int elemento){
+    this.elemento=elemento;
+    this.izquierda=null;
+    this.Derecha=null;
+    }
+}
